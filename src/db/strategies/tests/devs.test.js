94b16e6369e9937d3a80af1d4c9d819b099d78c5
wellington santos <weltossousa@gmail.com>
2020-02-21 23:23:58 -0300

    configurando banco de dados postgres e execuntando todos os casos de tests

---
+++ src/db/strategies/tests/devs.test.js
@@ -0,0 +1,59 @@
+const assert = require('assert')
+const Postgres = require('../postgres/postegres')
+const ContextPostgres = require('../base/contextStrategy')
+const schemaDevs = require('../postgres/schemas/devSchema')
+let context = {}
+
+const DEV_DEFAULT = {
+    name: 'wellington',
+    github_username: 'wellingtonn96',
+    bio: 'sou um profissional muito capaz',
+    avatar_url: 'https://localhost:3000/devs',
+    techs: 'Nodejs, Reatjs e React Native',
+    location: [1.545, 1.545],
+    created_at: new Date(),
+    updated_at: new Date()
+}
+
+const DEV_DEFAULT_ATUALIZAR = {
+    name: 'Diego Fernades',
+    github_username: 'Diego@Rocketseat',
+    bio: 'sou um profissional muito capaz',
+    avatar_url: 'https://localhost:3000/devs',
+    techs: 'Nodejs, Reatjs e React Native',
+    location: [1.545, 1.545],
+    created_at: new Date(),
+    updated_at: new Date()
+}
+
+describe('swite de tests devs', () => {
+    before(async() => {
+        const connection = await Postgres.connect()
+        const model = await Postgres.defineModel(connection, schemaDevs)
+        context = new ContextPostgres(new Postgres(model))
+    })
+
+    it('cadastrar um dev', async() => {
+        const { dataValues } = await context.create(DEV_DEFAULT)
+        delete dataValues.id
+        assert.deepEqual(dataValues, DEV_DEFAULT) 
+    })
+
+    it('listar devs', async() => {
+        const [results] = await context.read(DEV_DEFAULT);
+        delete results.id
+        assert.deepEqual(results, DEV_DEFAULT)
+    })
+
+    it('atualizar um dev', async() => {
+        const [item] = await context.read()
+        const [results] = await context.update(item.id, DEV_DEFAULT_ATUALIZAR) 
+        assert.deepEqual(results, 1)
+    })
+
+    it('excluir um dev', async() => {
+        const [item] = await context.read()
+        const results = await context.delete(item.id)
+        assert.deepEqual(results, 1)
+    })
+})
